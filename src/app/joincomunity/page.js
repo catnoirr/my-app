564889e005fdeb4dc013f09726b5035dac8f38df
offxsagr <hakies.06@gmail.com>
2024-11-08 10:33:55 +0530

    swipper pendinfg

--- src/app/joincomunity/page.js
+++ src/app/joincomunity/page.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Hero from '../components/HeroSection.jsx'; // Adjust the path if `Hero.js` is in a different directory
 import Section from '../components/Section'; // Adjust the path if `Hero.js` is in a different directory
 import SecondSection from '../components/Section2'; // Adjust the path if `Hero.js` is in a different directory
-
+import SponsorshipSection from '../components/SponsorshipSection.jsx'
+import Testimonials from '../components/Solved.jsx'
 const Page = () => {
   return (
     <div className='bg-gray-100'>
@@ -10,6 +11,8 @@ const Page = () => {
      
       <SecondSection/>
        <Section/>
+       <SponsorshipSection/>
+       <Testimonials/>
     </div>
   );
 };
